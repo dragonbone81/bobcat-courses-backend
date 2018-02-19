011507a9b056ae1ee0cdaebf85d3c60644ce6969
Christian <cvernikoff@gmail.com>
2018-02-19 15:45:44 -0800

    change to just a url call

--- course_api/scheduler_tasks/tasks.py
+++ course_api/scheduler_tasks/tasks.py
@@ -1,23 +1,17 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
-from course_api.data_managers.course_pull import UCMercedClassParser
-from course_api.models import Course
-import os
-import django
+import requests
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "course_planner.settings")
-django.setup()
 sched = BlockingScheduler()
 
 
 @sched.scheduled_job('interval', minutes=1)
 def timed_job():
-    # Todo prob move this to data_managers
-    data = UCMercedClassParser('201810').parse()
-    for course in data:
-        course_data = course
-        course_data.pop('crn')
-        Course.objects.update_or_create(crn=course.get('crn'), defaults=course_data)
-    print('Course Pull Ran')
+    # Todo prob move this to data_managers and figure this out
+    response = requests.get(url="https://cse120-course-planner.herokuapp.com/course_pull", params={'pull': True}).json()
+    if response.get('success'):
+        print('Course Pull Ran')
+    else:
+        print('Course Pull Failed')
 
 
 sched.start()
--- course_api/views.py
+++ course_api/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+
+from course_api.data_managers.course_pull import UCMercedClassParser
+from course_api.models import Course
+
 
 # Create your views here.
+def course_view(request):
+    if request.GET and request.GET.get('pull'):
+        data = UCMercedClassParser('201810').parse()
+        for course in data:
+            # TODO weird thing with python and not references
+            crn = course.get('crn')
+            course.pop('crn')
+            Course.objects.update_or_create(crn=crn, defaults=course)
+    return JsonResponse({'success': True})
