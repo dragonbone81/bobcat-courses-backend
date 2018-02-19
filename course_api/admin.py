4f5d9d5d16294f569bcd221c2c36fbd79ce822b1
Christian <cvernikoff@gmail.com>
2018-02-19 13:38:52 -0800

    screwed up on model class

--- course_api/admin.py
+++ course_api/admin.py
@@ -5,7 +5,6 @@ from course_api.models import Course
 # Register your models here.
 class CourseAdmin(admin.ModelAdmin):
     fields = ['crn', 'subject', 'course_name']
-    filter_horizontal = ['subject', 'term']
     search_fields = ['crn', 'subject', 'course_name']
 
 
