6b35ce10c0559ce99d8e849581383b1d77687c9f
Christian <cvernikoff@gmail.com>
2018-02-19 15:28:16 -0800

    tassk with dumb django

--- course_api/scheduler_tasks/tasks.py
+++ course_api/scheduler_tasks/tasks.py
@@ -1,7 +1,9 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 from course_api.data_managers.course_pull import UCMercedClassParser
 from course_api.models import Course
+import os
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "course_planner.settings")
 sched = BlockingScheduler()
 
 
