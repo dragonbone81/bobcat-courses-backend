678e9bdc0739de46ea81b25700921ffb0de18db8
Christian <cvernikoff@gmail.com>
2018-02-19 14:52:40 -0800

    fix migrations + good admin

--- course_api/admin.py
+++ course_api/admin.py
@@ -3,10 +3,9 @@ from course_api.models import Course
 
 
 # Register your models here.
-# class CourseAdmin(admin.ModelAdmin):
-    # list_display = ('crn', 'subject', 'course_name')
-    # search_fields = ['crn', 'subject', 'course_name']
-    # pass
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('crn', 'subject', 'course_name')
+    search_fields = ['crn', 'subject', 'course_name']
 
 
-admin.site.register(Course)
+admin.site.register(Course, CourseAdmin)
--- course_api/models.py
+++ course_api/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 
-# Create your models here.
 class Course(models.Model):
     class Meta:
         verbose_name = "Course"
@@ -12,72 +11,72 @@ class Course(models.Model):
         max_length=32,
         db_index=True,
         verbose_name="CRN",
-    ),
+    )
     subject = models.CharField(
         max_length=256,
         null=True,
         blank=True,
         db_index=True,
         verbose_name="Subject",
-    ),
+    )
     course_id = models.CharField(
         max_length=256,
         null=True,
         blank=True,
         db_index=True,
         verbose_name="Course ID",
-    ),
+    )
     course_name = models.CharField(
         max_length=256,
         null=True,
         blank=True,
         db_index=True,
         verbose_name="Course Name",
-    ),
+    )
     units = models.IntegerField(
         null=True,
         blank=True,
         db_index=True,
         verbose_name="Units",
-    ),
+    )
     type = models.CharField(
         max_length=32,
         null=True,
         blank=True,
         db_index=True,
         verbose_name="Type",
-    ),
+    )
     days = models.CharField(
         max_length=32,
         null=True,
         blank=True,
         db_index=True,
-    ),
+    )
     hours = models.CharField(
         max_length=32,
         null=True,
         blank=True,
         db_index=True,
-    ),
+    )
     room = models.CharField(
         max_length=32,
         null=True,
         blank=True,
         db_index=True,
-    ),
+    )
     dates = models.CharField(
         max_length=256,
         null=True,
         blank=True,
         db_index=True,
-    ),
+    )
     instructor = models.CharField(
         max_length=256,
         null=True,
         blank=True,
         db_index=True,
         verbose_name="Instructor",
-    ),
+    )
     # TODO needs work
     lecture_crn = models.CharField(
         max_length=256,
@@ -85,89 +84,92 @@ class Course(models.Model):
         blank=True,
         db_index=True,
         verbose_name="Lecture CRN",
-    ),
+    )
     term = models.CharField(
         max_length=256,
         null=True,
         blank=True,
         db_index=True,
         verbose_name="Term",
-    ),
+    )
     capacity = models.IntegerField(
         null=True,
         blank=True,
         db_index=True,
         verbose_name="Capacity",
-    ),
+    )
     enrolled = models.IntegerField(
         null=True,
         blank=True,
         db_index=True,
         verbose_name="Enrolled",
-    ),
+    )
     available = models.IntegerField(
         null=True,
         blank=True,
         db_index=True,
         verbose_name="Available",
-    ),
+    )
     final_type = models.CharField(
         max_length=256,
         null=True,
         blank=True,
         db_index=True,
-    ),
+    )
     final_days = models.CharField(
         max_length=256,
         null=True,
         blank=True,
         db_index=True,
-    ),
+    )
     final_hours = models.CharField(
         max_length=256,
         null=True,
         blank=True,
         db_index=True,
-    ),
+    )
     final_room = models.CharField(
         max_length=256,
         null=True,
         blank=True,
         db_index=True,
-    ),
+    )
     final_dates = models.CharField(
         max_length=256,
         null=True,
         blank=True,
         db_index=True,
-    ),
+    )
     final_type_2 = models.CharField(
         max_length=256,
         null=True,
         blank=True,
         db_index=True,
-    ),
+    )
     final_days_2 = models.CharField(
         max_length=256,
         null=True,
         blank=True,
         db_index=True,
-    ),
+    )
     final_hours_2 = models.CharField(
         max_length=256,
         null=True,
         blank=True,
         db_index=True,
-    ),
+    )
     final_room_2 = models.CharField(
         max_length=256,
         null=True,
         blank=True,
         db_index=True,
-    ),
+    )
     final_dates_2 = models.CharField(
         max_length=256,
         null=True,
         blank=True,
         db_index=True,
-    ),
+    )
+
+    def __str__(self):
+        return self.crn
