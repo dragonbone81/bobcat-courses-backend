6e377d56647b5f4b389ae50b7f788d908934e671
Christian <cvernikoff@gmail.com>
2018-02-19 15:16:39 -0800

    course pull task

--- course_api/scheduler_tasks/tasks.py
+++ course_api/scheduler_tasks/tasks.py
@@ -1,12 +1,18 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 from course_api.data_managers.course_pull import UCMercedClassParser
+from course_api.models import Course
 
 sched = BlockingScheduler()
 
 
 @sched.scheduled_job('interval', minutes=1)
 def timed_job():
-    UCMercedClassParser('201810').parse()
+    # Todo prob move this to data_managers
+    data = UCMercedClassParser('201810').parse()
+    for course in data:
+        course_data = course
+        course_data.pop('crn')
+        Course.objects.update_or_create(crn=course.get('crn'), defaults=course_data)
     print('Course Pull Ran')
 
 
