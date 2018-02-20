84dd2ab63ad44d48ffef240b4927720f33e1ddbf
Christian <cvernikoff@gmail.com>
2018-02-19 18:31:45 -0800

    scheduler

--- course_api/__init__.py
+++ course_api/__init__.py
@@ -1,1 +1,3 @@
+from course_api.scheduler_tasks.tasks import timed_job
+
 default_app_config = 'course_api.apps.CourseApiConfig'
--- course_api/scheduler_tasks/tasks.py
+++ course_api/scheduler_tasks/tasks.py
@@ -1,15 +1,13 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 from course_api.data_managers.course_push import UCMercedCoursePush
-from django_apscheduler.jobstores import DjangoJobStore
 
 scheduler = BackgroundScheduler()
-scheduler.add_jobstore(DjangoJobStore(), 'djangojobstore')
 
 
 @scheduler.scheduled_job('interval', hours=6, id="course_update")  # to not annoy or whatever
 def timed_job():
     # Todo prob move this to data_managers and figure this out
-    UCMercedCoursePush().push_courses()()
+    UCMercedCoursePush().push_courses()
     print('Course Pull Ran')
 
 
