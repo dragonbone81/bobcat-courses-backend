460877821c021a947cfc043948a0e3cd6445389e
Christian <cvernikoff@gmail.com>
2018-02-19 18:09:08 -0800

    scheduler

--- course_api/views.py
+++ course_api/views.py
@@ -20,6 +20,12 @@ class CourseViewSet(viewsets.ModelViewSet):
 
 
 class Scheduler(object):
+    import os
+
+    from django.core.wsgi import get_wsgi_application
+
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'course_planner.settings'
+    application = get_wsgi_application()
     from apscheduler.schedulers.background import BackgroundScheduler
     sched = BackgroundScheduler()
 
