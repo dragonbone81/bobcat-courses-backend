7c0fa04ac1658f6dd4d4c98f65d8fe89017113cd
Christian <cvernikoff@gmail.com>
2018-02-19 19:07:22 -0800

    scheduler i give up

--- course_api/scheduler_tasks/tasks.py
+++ course_api/scheduler_tasks/tasks.py
@@ -4,7 +4,8 @@ import requests
 scheduler = BlockingScheduler()
 
 
-@scheduler.scheduled_job('interval', minutes=1)  # to not annoy or whatever
+@scheduler.scheduled_job('interval', hours=6)  # TODO figure this out so that it will run often during high demand
+# TODO idk how to do this using real scheduling and it times out
 def timed_job():
     # Todo prob move this to data_managers and figure this out
     url = "https://cse120-course-planner.herokuapp.com/course_pull"
