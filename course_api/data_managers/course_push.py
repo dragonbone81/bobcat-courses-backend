9ff09a5be21d2743c89366765f0ed2d0d1ce9ade
Christian <cvernikoff@gmail.com>
2018-02-20 10:41:24 -0800

    improved performance

--- course_api/data_managers/course_push.py
+++ course_api/data_managers/course_push.py
@@ -19,13 +19,15 @@ class UCMercedCoursePush(object):
             else:
                 course['available'] = int(course['available'])
             course_object, created = Course.objects.update_or_create(crn=crn, defaults=course)
-            course_object.lecture = None
-            course_object.discussion = None
-            if course.get('lecture_crn'):
-                lecture, created = Course.objects.update_or_create(crn=course.get('lecture_crn'))
-                course_object.lecture = lecture
-            if course.get('discussion_crn'):
-                discussion, created = Course.objects.update_or_create(crn=course.get('discussion_crn'))
-                course_object.discussion = discussion
-            print(course_object.crn)
-            course_object.save()
+
+            # TODO remove to improve performance and speed so the http doesnt time out while we figure out actual tasks
+            # course_object.lecture = None
+            # course_object.discussion = None
+            # if course.get('lecture_crn'):
+            #     lecture, created = Course.objects.update_or_create(crn=course.get('lecture_crn'))
+            #     course_object.lecture = lecture
+            # if course.get('discussion_crn'):
+            #     discussion, created = Course.objects.update_or_create(crn=course.get('discussion_crn'))
+            #     course_object.discussion = discussion
+            # print(course_object.crn)
+            # course_object.save()
