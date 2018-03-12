87a4d7b9131843e32fee20b581077729929bd64f
Maxime Moison <maximaxof@me.com>
2018-03-11 23:07:52 -0700

    Now can filter out schedules that have conflicts

--- course_api/data_managers/course_scheduler.py
+++ course_api/data_managers/course_scheduler.py
@@ -13,7 +13,15 @@ def convertTime(s):
     return { "start": int(startTime[0] + startTime[1]), "end": int(endTime[0] + endTime[1]) }
 
 def getAllCoursesForCourseID(id):
-    courses = { "cse120":[{"course_id": "cse-120-01", "type": "LECT", "crn": 1}, {"course_id": "cse-120-02L", "type": "LAB", "crn": 2, "lecture_crn":1}, {"course_id": "cse-120-03L", "type": "LAB", "crn": 3, "lecture_crn":1}], "cse150":[{"course_id": "cse-150-01", "type": "LECT", "crn": 4}, {"course_id": "cse-150-02L", "type": "LAB", "crn": 5, "lecture_crn":4}, {"course_id": "cse-150-03L", "type": "LAB", "crn": 6, "lecture_crn":4}]}
+    courses = {
+"cse120":[
+          {"course_id": "cse-120-01", "days": "M", "hours": "11:30-12:20pm", "type": "LECT", "crn": 1},
+          {"course_id": "cse-120-02L", "days": "R", "hours": "11:30-12:20pm", "type": "LAB", "crn": 2, "lecture_crn":1},
+          {"course_id": "cse-120-03L", "days": "W", "hours": "11:30-12:20pm", "type": "LAB", "crn": 3, "lecture_crn":1}],
+"cse150":[
+          {"course_id": "cse-150-01", "days": "T", "hours": "11:30-12:20pm", "type": "LECT", "crn": 4},
+          {"course_id": "cse-150-02L", "days": "W", "hours": "11:30-12:20pm", "type": "LAB", "crn": 5, "lecture_crn":4},
+          {"course_id": "cse-150-03L", "days": "W", "hours": "11:30-12:20pm", "type": "LAB", "crn": 6, "lecture_crn":4}]}
     return courses[id]
 
 def getCourse(crn, courses):
@@ -59,9 +67,54 @@ def generateSchedules(courseIDs):
         for c in classes:
             sectionKey = classes[c].keys()[i/n % len(classes[c])]
             permutations[i][c] = classes[c][sectionKey]
-            n *= len(c)
+            n *= len(classes[c])
     return permutations
 
+def dayConflicts(time, day):
+    for t in day:
+        if time["start"] >= t["start"] and time["start"] <= t["end"]:
+            return True
+        if time["end"] >= t["start"] and time["end"] <= t["end"]:
+            return True
+    return False
+
+
+
+
+def hasConflict(schedule):
+    times ={"M":[], "T":[], "W":[], "R":[], "F":[], "S":[]}
+    finals = {"M":[], "T":[], "W":[], "R":[], "F":[], "S":[]}
+    allCourses = []
+    for c in schedule:
+        if schedule[c]["LECT"]:
+            allCourses.append(schedule[c]["LECT"])
+        
+        if schedule[c]["LAB"]:
+            allCourses.append(schedule[c]["LAB"])
+
+        if schedule[c]["DISC"]:
+            allCourses.append(schedule[c]["DISC"])
+        
+    for c in allCourses:
+        for day in c["days"]:
+            time = convertTime(c["hours"])
+            if not dayConflicts(time, times[day]):
+                times[day].append(time)
+            else:
+                return True
+
+    return False
+
+
+
+
+
+
+
 perms = generateSchedules(["cse120", "cse150"])
+
 for p in perms:
-    print p
+    if not hasConflict(p):
+        for c in p:
+            print p[c]
+        print ("###########################")
