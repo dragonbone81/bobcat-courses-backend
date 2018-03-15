a1344e460c1324aab94ba0f41f25697027292905
Christian <cvernikoff@gmail.com>
2018-03-14 17:22:45 -0700

    added available filter to schedule search

--- course_api/utils/get_courses_base_on_simple_name.py
+++ course_api/utils/get_courses_base_on_simple_name.py
@@ -6,5 +6,5 @@ def get_courses(courses_to_search, term):
     courses = dict()
     for course in courses_to_search:
         courses[course] = [CourseSerializer(course).data for course in
-                           Course.objects.filter(simple_name__iexact=course, term=term)]
+                           Course.objects.filter(simple_name__iexact=course, term=term, available__gt=0)]
     return courses
