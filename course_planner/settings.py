8167f309201e8ab3a7969cf5ebd10da6257d1299
Christian <cvernikoff@gmail.com>
2018-03-24 10:53:23 -0700

    debug off

--- course_planner/settings.py
+++ course_planner/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY', 'DEV_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-if os.environ.get('DEBUG'):
+if os.environ.get('DEBUG') == 'PRODUCTION_OFF':
     DEBUG = False
 else:
     DEBUG = True
