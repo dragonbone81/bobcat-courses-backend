55aed41fbbdcd066fa2d11a64d33e454efa6ee32
Christian <cvernikoff@gmail.com>
2018-03-30 10:19:00 -0700

    plxzzzzzz

--- course_planner/settings.py
+++ course_planner/settings.py
@@ -22,10 +22,15 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'DEV_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 if os.environ.get('DEBUG') == 'PRODUCTION_OFF':
     DEBUG = False
-    secrets = {}
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_STORAGE_BUCKET_NAME = os.environ.get('S3_BUCKET_NAME')
 else:
     from course_planner.secrets import AWS_KEYS
 
+    AWS_ACCESS_KEY_ID = AWS_KEYS.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = AWS_KEYS.get('AWS_SECRET_ACCESS_KEY')
+    AWS_STORAGE_BUCKET_NAME = AWS_KEYS.get('S3_BUCKET_NAME')
     DEBUG = True
 
 ALLOWED_HOSTS = ['cse120-course-planner.herokuapp.com', '.bobcat-courses.me']
@@ -148,9 +153,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = os.environ.get('S3_BUCKET_NAME')
+
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
@@ -160,5 +163,3 @@ DEFAULT_FILE_STORAGE = 'course_planner.storage_backends.MediaStorage'  # <-- her
 STATIC_URL = '/static/'
 
 django_heroku.settings(locals())
-
-
