c88065ecaf0020321872b2448931b7e9f5f7c4c1
Christian <cvernikoff@gmail.com>
2018-04-02 00:17:57 -0700

    celery

--- course_api/tasks.py
+++ course_api/tasks.py
@@ -1,11 +1,12 @@
 from celery import shared_task
-from course_api.data_managers.course_push import UCMercedCoursePush, SubjectClassUpdate
+# from course_api.data_managers.course_push import UCMercedCoursePush, SubjectClassUpdate
 
 
 @shared_task
 def name_of_your_function():
-    print("starting course_push")
-    UCMercedCoursePush().push_courses()
-    print("finished first push")
-    SubjectClassUpdate().update_lectures()
-    print("finished full push")
+    print('celery_task')
+    # print("starting course_push")
+    # UCMercedCoursePush().push_courses()
+    # print("finished first push")
+    # SubjectClassUpdate().update_lectures()
+    # print("finished full push")
