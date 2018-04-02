94b07196ebdce57e057b89e4d3008e2d9f4bba3a
Christian <cvernikoff@gmail.com>
2018-04-02 00:23:43 -0700

    celery

--- course_api/tasks.py
+++ course_api/tasks.py
@@ -1,12 +1,17 @@
+from __future__ import absolute_import, unicode_literals
 from celery import shared_task
-# from course_api.data_managers.course_push import UCMercedCoursePush, SubjectClassUpdate
 
 
 @shared_task
-def name_of_your_function():
-    print('celery_task')
-    # print("starting course_push")
-    # UCMercedCoursePush().push_courses()
-    # print("finished first push")
-    # SubjectClassUpdate().update_lectures()
-    # print("finished full push")
+def add(x, y):
+    return x + y
+
+
+@shared_task
+def mul(x, y):
+    return x * y
+
+
+@shared_task
+def xsum(numbers):
+    return sum(numbers)
