af5d3ac1d90cb89278e18861143839bd71d228d6
Christian <cvernikoff@gmail.com>
2018-04-16 13:19:00 -0700

    201830 course_pull

--- course_api/scheduler_tasks/tasks.py
+++ course_api/scheduler_tasks/tasks.py
@@ -1,10 +1,14 @@
 import requests
 
 
-def timed_job():
-    url = "https://cse120-course-planner.herokuapp.com/course_pull"
+# def timed_job():
+#     url = "https://cse120-course-planner.herokuapp.com/course_pull"
+#     requests.get(url=url)
+#     print('Course Pull Ran')
+def course_pull_201030():
+    url = "https://cse120-course-planner.herokuapp.com/course_pull/?pull=True&term=201830"
     requests.get(url=url)
-    print('Course Pull Ran')
+    print('Course Pull 201830 Ran')
 
 
-timed_job()
+course_pull_201030()
