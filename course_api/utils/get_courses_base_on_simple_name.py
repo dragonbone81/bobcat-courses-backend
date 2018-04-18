cdcf98ef19dac7eb5b69f99c6ba553b712b65eb9
Christian <cvernikoff@gmail.com>
2018-04-18 13:21:31 -0700

    fixed the thing i broke

--- course_api/utils/get_courses_base_on_simple_name.py
+++ course_api/utils/get_courses_base_on_simple_name.py
@@ -4,6 +4,7 @@ from django.db.models import Q
 
 def get_courses(courses_to_search, term, search_full=False, bad_crns=False):
     courses = dict()
+    crn_dict = dict()
     if not search_full:
         total_courses = Course.objects.filter(~Q(crn__in=bad_crns), simple_name__in=courses_to_search, term=term,
                                               available__gt=0)
@@ -12,23 +13,25 @@ def get_courses(courses_to_search, term, search_full=False, bad_crns=False):
     for course in total_courses:
         if not courses.get(course.simple_name):
             courses[course.simple_name] = []
-        courses[course.simple_name].append({
-            'crn': course.crn,
-            'type': course.type,
-            'course_id': course.course_id,
-            'lecture_crn': course.lecture_crn,
-            'hours': course.hours,
-            'days': course.days,
-            'final_days': course.final_days,
-            'final_hours': course.final_hours,
-            'simple_name': course.simple_name,
-            'subject': course.subject,
-            'course_name': course.course_name,
-            'term': course.term,
-            'capacity': course.capacity,
-            'enrolled': course.enrolled,
-            'available': course.available,
-            'instructor': course.instructor,
-            'units': course.units,
-        })
+        if (course.lecture_crn and crn_dict.get(course.lecture_crn)) or not course.lecture_crn:
+            crn_dict[course.crn] = course.simple_name
+            courses[course.simple_name].append({
+                'crn': course.crn,
+                'type': course.type,
+                'course_id': course.course_id,
+                'lecture_crn': course.lecture_crn,
+                'hours': course.hours,
+                'days': course.days,
+                'final_days': course.final_days,
+                'final_hours': course.final_hours,
+                'simple_name': course.simple_name,
+                'subject': course.subject,
+                'course_name': course.course_name,
+                'term': course.term,
+                'capacity': course.capacity,
+                'enrolled': course.enrolled,
+                'available': course.available,
+                'instructor': course.instructor,
+                'units': course.units,
+            })
     return courses
