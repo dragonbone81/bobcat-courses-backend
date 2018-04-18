15f1ae0fcd6b89b9433020dcb55a083f6ef134e0
Christian <cvernikoff@gmail.com>
2018-04-18 13:56:49 -0700

    fixed the thing again

--- course_api/utils/get_courses_base_on_simple_name.py
+++ course_api/utils/get_courses_base_on_simple_name.py
@@ -3,7 +3,7 @@ from django.db.models import Q
 
 
 def get_courses(courses_to_search, term, search_full=False, bad_crns=False):
-    courses = dict()
+    courses = {course: [] for course in courses_to_search}
     crn_dict = dict()
     if not search_full:
         total_courses = Course.objects.filter(~Q(crn__in=bad_crns), simple_name__in=courses_to_search, term=term,
@@ -11,27 +11,24 @@ def get_courses(courses_to_search, term, search_full=False, bad_crns=False):
     else:
         total_courses = Course.objects.filter(~Q(crn__in=bad_crns), simple_name__in=courses_to_search, term=term)
     for course in total_courses:
-        if not courses.get(course.simple_name):
-            courses[course.simple_name] = []
-        if (course.lecture_crn and crn_dict.get(course.lecture_crn)) or not course.lecture_crn:
-            crn_dict[course.crn] = course.simple_name
-            courses[course.simple_name].append({
-                'crn': course.crn,
-                'type': course.type,
-                'course_id': course.course_id,
-                'lecture_crn': course.lecture_crn,
-                'hours': course.hours,
-                'days': course.days,
-                'final_days': course.final_days,
-                'final_hours': course.final_hours,
-                'simple_name': course.simple_name,
-                'subject': course.subject,
-                'course_name': course.course_name,
-                'term': course.term,
-                'capacity': course.capacity,
-                'enrolled': course.enrolled,
-                'available': course.available,
-                'instructor': course.instructor,
-                'units': course.units,
-            })
+        crn_dict[course.crn] = course.simple_name
+        courses[course.simple_name].append({
+            'crn': course.crn,
+            'type': course.type,
+            'course_id': course.course_id,
+            'lecture_crn': course.lecture_crn,
+            'hours': course.hours,
+            'days': course.days,
+            'final_days': course.final_days,
+            'final_hours': course.final_hours,
+            'simple_name': course.simple_name,
+            'subject': course.subject,
+            'course_name': course.course_name,
+            'term': course.term,
+            'capacity': course.capacity,
+            'enrolled': course.enrolled,
+            'available': course.available,
+            'instructor': course.instructor,
+            'units': course.units,
+        })
     return courses
