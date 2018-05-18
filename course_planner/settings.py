0706eb0c79b83159a34d22b27ee28d4035b68027
Christian <cvernikoff@gmail.com>
2018-05-18 15:33:38 -0700

    dirrrrs

--- course_planner/settings.py
+++ course_planner/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY', 'DEV_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-print(os.environ)
 if os.environ.get('DEBUG') == 'PRODUCTION_OFF':
     DEBUG = False
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
@@ -133,6 +132,9 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+import dj_database_url
+
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
