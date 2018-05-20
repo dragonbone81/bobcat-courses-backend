6a59f7584ff6e724488dea4d9bd88d0ee1b4aac0
Christian <cvernikoff@gmail.com>
2018-05-19 18:50:48 -0700

    set a max of notifications to 20

--- course_api/signals.py
+++ course_api/signals.py
@@ -1,10 +1,11 @@
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 from course_api.models import ScheduleUser, Notifications
 from random import choices
 from string import ascii_uppercase, digits
 from django.dispatch import receiver
 from json import dumps as json_dumps
+from json import loads as json_loads
 
 
 @receiver(post_save, sender=User)
@@ -19,3 +20,11 @@ def save_profile(sender, instance, raw, using, update_fields, **kwargs):
                 {'seen': False, 'type': 'message', 'id': 0, 'data': {'message': 'Welcome to BobcatCourses'}}
             ])
         )
+
+
+@receiver(pre_save, sender=Notifications)
+def save_notifications(sender, instance, raw, using, update_fields, **kwargs):
+    notifications = json_loads(instance.notifications)
+    if len(notifications) > 20:
+        del notifications[0]
+    instance.notifications = json_dumps(notifications)
