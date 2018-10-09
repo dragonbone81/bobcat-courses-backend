2beaf428ac8ce47e5462f036866e3cfb798f7fa4
Christian <cvernikoff@gmail.com>
2018-10-08 22:39:44 -0700

    updated django to fix security vul

--- course_api/utils/convert_24h.py
+++ course_api/utils/convert_24h.py
@@ -11,26 +11,3 @@ def convertTime(s):
     if int(startTime[0]) > int(endTime[0]):  # if for some reason it starts (it means it started in the morning
         startTime[0] = str(int(startTime[0]) - 12)  # remove 12 hours
     return {"start": int(startTime[0] + startTime[1]), "end": int(endTime[0] + endTime[1])}  # return {start, end}
-
-
-def clamp(val, minimum=0, maximum=255):
-    if val < minimum:
-        return int(minimum)
-    if val > maximum:
-        return int(maximum)
-    return int(val)
-
-
-def colorscale(hexstr, scalefactor):
-    hexstr = hexstr.strip('#')
-
-    if scalefactor < 0 or len(hexstr) != 6:
-        return hexstr
-
-    r, g, b = int(hexstr[:2], 16), int(hexstr[2:4], 16), int(hexstr[4:], 16)
-
-    r = clamp(r * scalefactor)
-    g = clamp(g * scalefactor)
-    b = clamp(b * scalefactor)
-
-    return "#%02x%02x%02x" % (r, g, b)
