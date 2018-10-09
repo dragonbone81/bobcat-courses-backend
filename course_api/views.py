249db2922bd5c486683ad1743f15da8975b1de5c
Christian <cvernikoff@gmail.com>
2018-10-09 10:59:18 -0700

    put the push back

--- course_api/views.py
+++ course_api/views.py
@@ -214,9 +214,8 @@ class GetTerms(ViewSet):
 # Create your views here.
 def course_view(request):
     if request.GET and request.GET.get('pull'):
-        Course.objects.filter(term='201810').delete()
-        # term = request.GET.get('term')
-        # UCMercedCoursePush(terms=[str(term)]).push_courses()
+        term = request.GET.get('term')
+        UCMercedCoursePush(terms=[str(term)]).push_courses()
     elif request.GET and request.GET.get('simple'):
         SubjectClassUpdate().update_lectures()
     elif request.GET and request.GET.get('delete'):
