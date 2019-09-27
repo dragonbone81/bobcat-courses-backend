66f514084d180eb97acdfcbdf8688e10a945e26c
dragonbone81 <cvernikoff@gmail.com>
2019-09-27 16:09:39 -0700

    Update settings.py

--- course_planner/settings.py
+++ course_planner/settings.py
@@ -21,7 +21,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'DEV_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 if os.environ.get('DEBUG') == 'PRODUCTION_OFF':
-    DEBUG = True
+    DEBUG = False
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = os.environ.get('S3_BUCKET_NAME')
